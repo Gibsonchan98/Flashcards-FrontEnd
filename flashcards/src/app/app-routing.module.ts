50a07919f8a1cba73dbd3a966c4bd228bf234092
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-04-25 11:53:47 -0700

    added routing

--- flashcards/src/app/app-routing.module.ts
+++ flashcards/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClient } from '@angular/common/http'
+import { AboutComponent } from './about/about.component';
+import { CardComponent } from './card/card.component';
+import { CollectionsComponent } from './collections/collections.component';
+import { DeckComponent } from './deck/deck.component';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: ' ', component: LandingComponent}, 
+  { path: 'Library', component: CollectionsComponent}, 
+  { path: 'Card', component: CardComponent}, 
+  { path: 'Deck', component: DeckComponent}, 
+  { path: 'About', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
