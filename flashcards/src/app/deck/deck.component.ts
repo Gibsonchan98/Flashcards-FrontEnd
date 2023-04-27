fc73f94ffd3afc3d76f1dfed58ca9b5fde9d5695
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-04-27 14:40:43 -0700

    view deck and create card

--- flashcards/src/app/deck/deck.component.ts
+++ flashcards/src/app/deck/deck.component.ts
@@ -13,18 +13,49 @@ export class DeckComponent implements OnInit{
   deckCategory:any;
   numOfCards : any;
   deck : Array<Flashcard> = [];
+  addCard = false;
 
   constructor(private router : Router, private activatedRoute: ActivatedRoute, private service :FlashcardapiService){}
 
   ngOnInit(): void {
-    this.numOfCards = 3;
     this.activatedRoute.params.subscribe(params => {
-      console.log('params', params);
       this.deckCategory = params['category'];
+      this.getCards(this.deckCategory);
+    })
+  }
+  
+  getCards(category: any){
+    this.service.getAllCards().subscribe(data => {
+      if(data != null){
+        for(let i = 0; i < data.length; i++){
+          if(category == data[i].category){
+            let card = {} as Flashcard;
+            card = data[i];
+            console.log(card);
+            this.deck.push(data[i]);
+          }
+        }
+        this.numOfCards = this.deck.length;
+      }
     })
   }
 
   goBackToLibrary(){
     this.router.navigateByUrl('Library');
   }
+
+  deleteCard(id : any){
+    console.log("delete " + id);
+  }
+
+  editCard(id : any, question : any, answer : any, correct : any){
+    console.log("delete " + id);
+  }
+
+  addNewCard(){
+    this.addCard = true;
+    console.log(this.addCard);
+  }
+  
+
 }
