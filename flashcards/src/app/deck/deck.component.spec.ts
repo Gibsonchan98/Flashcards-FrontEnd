db9329891618c238f7a79c4d9f94da818315ff0d
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-05-01 02:43:56 -0700

    landing testing

--- flashcards/src/app/deck/deck.component.spec.ts
+++ flashcards/src/app/deck/deck.component.spec.ts
@@ -1,23 +1,103 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute, convertToParamMap, Router } from '@angular/router';
+import { of } from 'rxjs';
 import { DeckComponent } from './deck.component';
+import { FlashcardapiService } from '../flashcardapi.service';
+import { Flashcard } from '../interfaces/Flashcard';
 
 describe('DeckComponent', () => {
   let component: DeckComponent;
   let fixture: ComponentFixture<DeckComponent>;
+  let router: Router;
+  let service: FlashcardapiService;
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DeckComponent ]
+      declarations: [ DeckComponent ],
+      imports: [ RouterTestingModule ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of(convertToParamMap({
+              category: 'category'
+            }))
+          }
+        }
+      ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(DeckComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
+    service = TestBed.inject(FlashcardapiService);
+    activatedRoute = TestBed.inject(ActivatedRoute);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get cards', () => {
+    const mockData = [
+      { id: 1, question: 'What is your name?', answer: 'My name is John', category: 'category', correct: false },
+      { id: 2, question: 'What is your age?', answer: 'I am 30 years old', category: 'category', correct: false }
+    ] as Flashcard[];
+    spyOn(service, 'getAllCards').and.returnValue(of(mockData));
+    component.getCards('category');
+    expect(component.deck.length).toBe(2);
+  });
+
+  it('should delete card', () => {
+    const spy = spyOn(service, 'deleteCard').and.returnValue(of(true));
+    spyOn(window, 'confirm').and.returnValue(true);
+    spyOn(router, 'navigateByUrl');
+    component.deleteCard(1);
+    expect(spy).toHaveBeenCalled();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/Deck/category');
+  });
+
+  it('should edit card', () => {
+    const mockCard = { id: 1, question: 'What is your name?', answer: 'My name is John', category: 'category', correct: false } as Flashcard;
+    component.editCard(mockCard);
+    expect(component.editedCard).toBe(mockCard);
+    expect(component.edit).toBeTrue();
+    expect(component.deckD).toBeFalse();
+  });
+
+  it('should add new card', () => {
+    component.addNewCard();
+    expect(component.addCard).toBeTrue();
+  });
+
+  it('should save card', () => {
+    const mockCard = { question: 'What is your name?', answer: 'My name is John', category: 'category', correct: false } as Flashcard;
+    const spy = spyOn(service, 'createCard').and.returnValue(of(mockCard));
+    const navigateByUrlSpy = spyOn(router, 'navigateByUrl');
+    component.saveCard('What is your name?', 'My name is John');
+    expect(spy).toHaveBeenCalled();
+    expect(navigateByUrlSpy).toHaveBeenCalledWith('/Deck/category');
+  });
+
+  it('should cancel', () => {
+    component.cancel();
+    expect(component.addCard).toBeFalse();
+    expect(component.edit).toBeFalse();
+  });
+
+  it('should save edit', () => {
+    const mockCard = { question: 'What is your name?', answer: 'My name is John', category: 'category', correct: false } as Flashcard;
+    const spy = spyOn(service, 'updateCard').and.returnValue(of(mockCard));
+    const navigateByUrlSpy = spyOn(router, 'navigateByUrl');
+    component.saveEdit('What is your name?', 'My name is John', 10);
+    expect(spy).toHaveBeenCalled();
+    expect(navigateByUrlSpy).toHaveBeenCalledWith('/Deck/category');
+  });
+
 });
--- flashcards/src/app/landing/landing.component.spec.ts
+++ flashcards/src/app/landing/landing.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { LandingComponent } from './landing.component';
+import { FooterComponent } from '../footer/footer.component';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,10 +9,13 @@ describe('LandingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingComponent ]
+      imports: [RouterTestingModule],
+      declarations: [LandingComponent]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +23,38 @@ describe('LandingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the Flashy logo', () => {
+    const logoElement = fixture.nativeElement.querySelector('#flashcards');
+    expect(logoElement.src).toContain('/assets/images/vectores/flashcards.svg');
+  });
+
+  it('should display the app name', () => {
+    const nameElement = fixture.nativeElement.querySelector('#name');
+    expect(nameElement.textContent).toBe(' Flashy ');
+  });
+
+  it('should display the app description', () => {
+    const descriptionElement = fixture.nativeElement.querySelector('h2');
+    expect(descriptionElement.textContent).toBe(' A simple app that keeps track of flashcards');
+  });
+
+  it('should navigate to the Library page when the "View Library" button is clicked', () => {
+    spyOn(component['router'], 'navigateByUrl');
+    const viewButton = fixture.nativeElement.querySelector('#viewButton');
+    viewButton.click();
+    expect(component['router'].navigateByUrl).toHaveBeenCalledWith('Library');
+  });
+
+  it('should navigate to the Create page when the "Create New Deck" button is clicked', () => {
+    spyOn(component['router'], 'navigateByUrl');
+    const createButton = fixture.nativeElement.querySelector('#createButton');
+    createButton.click();
+    expect(component['router'].navigateByUrl).toHaveBeenCalledWith('Create');
+  });
+
+  it('should fade in the content on initialization', () => {
+    expect(fixture.nativeElement.querySelector('.Title').classList).toContain('show');
   });
 });
