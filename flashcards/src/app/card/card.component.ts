94e0bf4be7525b363e3441b1e29fbf0a134b1f0a
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-05-01 04:17:57 -0700

    next and back for flashcard work

--- flashcards/src/app/card/card.component.ts
+++ flashcards/src/app/card/card.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, Input} from '@angular/core';
+import { Flashcard } from '../interfaces/Flashcard';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  @Input() flashcard!:Flashcard[];
 
+  index:any = 0;
+  
+  goNext(){
+    if(this.index != this.flashcard.length){
+      this.index += 1;
+    }
+  }
+
+  goBack(){
+    if(this.index != 0){
+      this.index -= 1;
+    }
+  }
 }
